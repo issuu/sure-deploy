e8a3495376ad8d45f7f1526c8ebf126f0c3f12e8
Marek Kubica <marek@xivilization.net>
2018-01-03 14:50:03 +0100

    Implement configurable timeout and verbosity

--- src/cli/sure_deploy.ml
+++ src/cli/sure_deploy.ml
@@ -3,17 +3,21 @@ open Async
 
 module Stack = Lib.Swarm_types.Stack
 
-let timeout = Time.Span.of_sec 60.
-
-let main host port verbose stack () =
+let main host port timeout_seconds verbose stack () =
   let swarm = (host, port) in
+  let timeout = Time.Span.of_sec timeout_seconds in
+  let level = match verbose with
+    | true -> `Info
+    | false -> `Error
+  in
+  Log.Global.set_level level;
   let%bind services_or = Lib.Requests.services swarm stack in
   let services = Or_error.ok_exn services_or in
   let service_names = List.map services ~f:Lib.Swarm_types.Service.to_string |> String.concat ~sep:", " in
   Log.Global.info "Services detected in '%a' stack: %s" Stack.pp stack service_names;
   match%bind Lib.Monitor.wait_for_completion_with_timeout timeout swarm services with
   | `Timeout ->
-    Log.Global.error "Waiting for convergence of stack '%a' timed out after TODO seconds" Stack.pp stack;
+    Log.Global.error "Waiting for convergence of stack '%a' timed out after %.3f seconds" Stack.pp stack timeout_seconds;
     return @@ shutdown 1
   | `Result () ->
     Log.Global.info "Stack '%a' has converged" Stack.pp stack;
@@ -22,14 +26,16 @@ let main host port verbose stack () =
 let () =
   let stack_name = Command.Spec.Arg_type.create Lib.Swarm_types.Stack.of_string in
   Command.async
-    ~summary:"Return recommendation JSON on stdout"
+    ~summary:"Wait for convergence of stack deployment on Docker Swarm"
     Command.Spec.(
       empty
       +> flag "--host" (required string)
          ~doc:" Hostname to connect to"
       +> flag "--port" (optional_with_default 2375 int)
          ~doc:" Port to connect to"
-      +> flag "--verbose" (optional_with_default false bool)
+      +> flag "--timeout" (optional_with_default 600. float)
+         ~doc:" Maximum time to wait for convergence"
+      +> flag "--verbose" no_arg
          ~doc:" Display more status information"
       +> anon ("stack-name" %: stack_name))
     main
